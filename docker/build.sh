24406873d59fea590cae8b9f1998fbcb8781f475
Stephen R Pietrowicz <srp@illinois.edu>
2019-12-02 12:50:27 -0600

    Update release versions

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,8 @@
 export LSST_STACK_VERSION=7-stack-lsst_distrib-w_2019_45
 export LSSTTS_DEPLOY_ENV_VERSION=salobj_v5.0.0_xml_v4.4.1
 export DM_CONFIG_AT_VERSION=1.0.0-rc4
-export DM_ATARCHIVER_VERSION=1.0.0-rc6
+export DM_ATARCHIVER_VERSION=1.0.0-rc8
 export DM_CSC_BASE_VERSION=1.0.0-rc7
 export OODS_VERSION=1.0.0-rc1
-export DM_CSC_RELEASE=1.0.0-rc7
-docker-compose build base atbase atarchiver atcontroller
+export DM_CSC_RELEASE=1.0.0-rc2
+docker-compose build base atbase atarchiver atcontroller oods
