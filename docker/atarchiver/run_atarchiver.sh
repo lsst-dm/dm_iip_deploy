248127a41d6daa9420ca77e7f37c6f0daf1a1a15
Stephen R Pietrowicz <srp@illinois.edu>
2019-12-03 16:33:35 -0600

    Add setup and correct entry point for services

--- docker/atarchiver/run_atarchiver.sh
+++ docker/atarchiver/run_atarchiver.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-export DM_CONFIG_AT_DIR=/home/saluser/dm_config_at
+source /home/ARC/miniconda3/bin/activate
+source $OSPL_HOME/release.com
+export PYTHONPATH=/home/ARC/dm_csc_base/python:/home/ARC/dm_ATArchiver/python:$PYTHONPATH
+export PATH=/home/ARC/dm_csc_base/bin:/home/ARC/dm_ATArchiver/bin:$PATH
+export DM_CONFIG_AT_DIR=/home/ARC/dm_config_at
 LOGPATH=/tmp/ospl_logs.$$
 mkdir $LOGPATH
 export OSPL_LOGPATH=$LOGPATH
--- docker/atcontroller/run_atcontroller.sh
+++ docker/atcontroller/run_atcontroller.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+source /home/ARC/miniconda3/bin/activate
+source $OSPL_HOME/release.com
+export PYTHONPATH=/home/ARC/dm_csc_base/python:/home/ARC/dm_ATArchiver/python:$PYTHONPATH
+export PATH=/home/ARC/dm_csc_base/bin:/home/ARC/dm_ATArchiver/bin:$PATH
 LOGPATH=/tmp/ospl_logs.$$
 mkdir $LOGPATH
 export OSPL_LOGPATH=$LOGPATH
