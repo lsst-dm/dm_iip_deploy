cc8b2f5b8e6377823a8617c9533dc919ccc95132
Stephen R Pietrowicz <srp@illinois.edu>
2020-01-29 14:39:11 -0600

    Changes to deploy oods container

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,10 @@
-export LSST_STACK_VERSION=7-stack-lsst_distrib-w_2019_45
+export LSST_STACK_VERSION=7-stack-lsst_distrib-d_2020_01_29
 export LSSTTS_DEPLOY_ENV_VERSION=salobj_v5.0.0_xml_v4.4.1
 export DM_CONFIG_AT_VERSION=1.0.0-rc4
 export DM_ATARCHIVER_VERSION=1.0.0-rc8
 export DM_CSC_BASE_VERSION=1.0.0-rc7
 export OODS_VERSION=1.0.0-rc2
 export DM_CSC_RELEASE=1.0.0-rc2
-docker-compose build base atbase atarchiver atcontroller oods
+export OODS_CP_VERSION=`echo $OODS_VERSION | cut -d"-" -f2`-`echo $LSST_STACK_VERSION | cut -d"-" -f4`
+#docker-compose build base atbase atarchiver atcontroller oods
+docker-compose build oods
--- docker/oods/run_oods.sh
+++ docker/oods/run_oods.sh
@@ -3,4 +3,4 @@ mkdir /tmp/user_data
 export EUPS_USERDATA=/tmp/user_data
 source /opt/lsst/software/stack/loadLSST.bash
 setup -r /opt/lsst/ctrl_oods
-oods.py oods.yaml
+oods.py $1
