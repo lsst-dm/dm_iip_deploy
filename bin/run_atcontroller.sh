e151fed841a71d6bbc800b9a9eb30eb66144f1bb
Stephen R Pietrowicz <srp@illinois.edu>
2020-04-27 10:11:32 -0500

    Make LSST_DDS_DOMAIN settable on the command line for launch Update TS_SALOBJ version

--- bin/run_atcontroller.sh
+++ bin/run_atcontroller.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
-LOGPATH=/tmp/ospl_logs.$$
-mkdir $LOGPATH
-export OSPL_LOGPATH=$LOGPATH
-. /opt/lsst/dm_ATArchiver/bin/setup_atarchiver.sh
-/opt/lsst/dm_ATArchiver/bin/run_atarchive_controller.py
+
+if [ -z $1 ];
+then
+    echo "$0: missing argument: lsst_dds_domain container_version"
+    exit 1
+fi
+
+if [ -z $2 ];
+then
+    echo "$0: missing argument: lsst_dds_domain container_version"
+    exit 1
+fi
+
+lsst_dds_domain=$1
+container_version=$2
+
+docker run -d \
+    -u `id -u ARC`:`id -g ARC` \
+    --network=host \
+    -e "IIP_CONFIG_DIR=/home/ARC/config" \
+    -e "IIP_CREDENTIAL_DIR=/home/ARC/.lsst" \
+    -e "LSST_DDS_DOMAIN=$lsst_dds_domain" \
+    -v /home/ARC/config:/home/ARC/config \
+    -v /home/ARC/.lsst:/home/ARC/.lsst \
+    -v /var/log/iip:/var/log/iip \
+    -v /data:/data \
+    lsstdm/atcontroller:$container_version
--- bin/run_ccarchiver.sh
+++ bin/run_ccarchiver.sh
@@ -2,20 +2,27 @@
 
 if [ -z $1 ];
 then
-    echo "$0: missing argument: container_version"
+    echo "$0: missing argument: lsst_dds_domain container_version"
     exit 1
 fi
 
-container_version=$1
+if [ -z $2 ];
+then
+    echo "$0: missing argument: lsst_dds_domain container_version"
+    exit 1
+fi
+
+lsst_dds_domain=$1
+container_version=$2
 
 docker run -d \
     -u `id -u ARC`:`id -g ARC` \
     --network=host \
     -e "IIP_CONFIG_DIR=/home/ARC/config" \
     -e "IIP_CREDENTIAL_DIR=/home/ARC/.lsst" \
-    -e "LSST_DDS_DOMAIN=comcampath" \
+    -e "LSST_DDS_DOMAIN=$lsst_dds_domain" \
     -v /home/ARC/config:/home/ARC/config \
     -v /home/ARC/.lsst:/home/ARC/.lsst \
     -v /var/log/iip:/var/log/iip \
     -v /data:/data \
-    lsstdm/cccontroller:$container_version
+    lsstdm/ccarchiver:$container_version
