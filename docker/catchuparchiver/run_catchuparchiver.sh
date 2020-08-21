b10153f617b0fdad1f9fa43efbadc87eb74efe8f
Stephen R Pietrowicz <srp@illinois.edu>
2020-08-21 12:56:07 -0500

    Change "ARC" to "arc" as user name

--- docker/catchuparchiver/run_catchuparchiver.sh
+++ docker/catchuparchiver/run_catchuparchiver.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-source /home/ARC/miniconda3/bin/activate
+source /home/arc/miniconda3/bin/activate
 source $OSPL_HOME/release.com
-export PYTHONPATH=/home/ARC/dm_csc_base/python:/home/ARC/dm_CatchupArchiver/python:$PYTHONPATH
-export PATH=/home/ARC/dm_csc_base/bin:/home/ARC/dm_CatchupArchiver/bin:$PATH
-export DM_CATCHUPARCHIVER_DIR=/home/ARC/dm_CatchupArchiver
-export DM_CSC_BASE_DIR=/home/ARC/dm_csc_base
-export DM_CONFIG_CATCHUP_DIR=/home/ARC/dm_config_catchup
+export PYTHONPATH=/home/arc/dm_csc_base/python:/home/arc/dm_CatchupArchiver/python:$PYTHONPATH
+export PATH=/home/arc/dm_csc_base/bin:/home/arc/dm_CatchupArchiver/bin:$PATH
+export DM_CATCHUPARCHIVER_DIR=/home/arc/dm_CatchupArchiver
+export DM_CSC_BASE_DIR=/home/arc/dm_csc_base
+export DM_CONFIG_CATCHUP_DIR=/home/arc/dm_config_catchup
 LOGPATH=/tmp/ospl_logs.$$
 mkdir $LOGPATH
 export OSPL_LOGPATH=$LOGPATH
