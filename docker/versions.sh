503987608b33af64d1ca04b306a97a519860091c
Stephen R Pietrowicz <srp@illinois.edu>
2022-11-02 18:00:47 +0000

    Update versions

--- docker/versions.sh
+++ docker/versions.sh
@@ -18,8 +18,8 @@ export PIKA_VERSION=1.1.0
 export REDIS_VERSION=3.5.3
 
 # set all the git tags or tickets for the versions you want to use for the containers
-export CTRL_OODS_GIT=tags/7.3.1
-export DM_IIP_DEPLOY_GIT=tags/7.4.1
+export CTRL_OODS_GIT=tags/7.3.2
+export DM_IIP_DEPLOY_GIT=tags/7.4.2
 #
 # remove the "tags/" or "tickets/"; these new values will be used to label containers
 #
