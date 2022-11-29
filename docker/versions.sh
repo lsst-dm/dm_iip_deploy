7251df8531632edaa645f37a8985fa79c561e740
Stephen R Pietrowicz <srp@illinois.edu>
2022-11-29 23:00:28 +0000

    Update version of ctrl_oods

--- docker/versions.sh
+++ docker/versions.sh
@@ -9,7 +9,7 @@ export OPENSPLICE_VERSION="V6.10.4"
 export LSST_DDS_RESPONSIVENESS_TIMEOUT="15s"
 
 # set all the git tags or tickets for the versions you want to use for the containers
-export CTRL_OODS_GIT=tags/7.4.0
+export CTRL_OODS_GIT=tags/7.5.1
 export DM_IIP_DEPLOY_GIT=tags/7.5.0
 #
 # remove the "tags/" or "tickets/"; these new values will be used to label containers
