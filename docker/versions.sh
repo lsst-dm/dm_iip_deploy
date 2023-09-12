cff16fdb24ed506bd68b611850f5f6505547bf37
Stephen R Pietrowicz <srp@illinois.edu>
2023-09-12 00:15:45 +0000

    Update versions

--- docker/versions.sh
+++ docker/versions.sh
@@ -1,10 +1,10 @@
 # t&s base container version
 export OSPL_CYCLE_TAG=c0032
-export CYCLE_TAG=c0032
+export CYCLE_TAG=c0032.002
 export TS_DDSCONFIG_PACKAGE=v0.10.1
 export OPENSPLICE_VERSION=V6.10.4
 export LSST_DDS_RESPONSIVENESS_TIMEOUT=15s
 
 # set all the git tags or tickets for the versions you want to use for the containers
-export CTRL_OODS_GIT=tags/8.0.0
-export DM_IIP_DEPLOY_GIT=tags/8.0.0
+export CTRL_OODS_GIT=tags/8.2.0
+export DM_IIP_DEPLOY_GIT=tags/8.2.0
